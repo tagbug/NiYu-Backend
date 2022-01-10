bba514e5b846bc104e65ef9841aa22c6e3eda57a
反话 <72679829+Zhongpeiqi@users.noreply.github.com>
2022-01-10 06:22:46 -0600

    Add files via upload

---
+++ NiYu-Backend/src/main/java/com/niyu/NiyuApplication.java
@@ -0,0 +1,13 @@
+package com.niyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiyuApplication.class, args);
+	}
+
+}
---
+++ NiYu-Backend/src/test/java/com/niyu/NiyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
